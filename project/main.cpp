3d7d851256c2c89b64276a07ade866389321a56f
kento <k023g0033@g.neec.ac.jp>
2024-12-20 11:01:51 +0900

    一度クリーンにした

--- project/main.cpp
+++ project/main.cpp
@@ -111,6 +111,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	objectCommon->SetDefaultCamera(cameraManager->GetActiveCamera());
 
+	//パーティクルマネージャーの初期化
+	ParticleManager::GetInstance()->Initialize(dxCommon, srvManager.get());
+
 	///////////////////////////////////////////////////////////////////////
 	///						>>>変数の宣言<<<								///
 	///////////////////////////////////////////////////////////////////////
@@ -125,21 +128,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	#pragma endregion
 
 	#pragma region 宣言と初期化
-	//スプライトの初期化
-	std::unique_ptr<Sprite> sprite = std::make_unique<Sprite>();
-	sprite->Initialize(spriteCommon,"./Resources/uvChecker.png");
-
-	//スプライト（複数）
-	std::vector<std::unique_ptr<Sprite>> sprites;
-	int spriteCount = 5;
-	for(int i = 0; i < spriteCount; ++i)
-	{
-		std::unique_ptr<Sprite> sprite = std::make_unique<Sprite>();
-		sprite->Initialize(spriteCommon,"./Resources/uvChecker.png");
-		sprite->SetSize({ 100.0f,100.0f });
-		sprite->SetPosition({ 200.0f * i,0.0f });
-		sprites.push_back(std::move(sprite));
-	}
 	#pragma endregion
 
 	/*--------------[ 3Dオブジェクト ]-----------------*/
@@ -152,18 +140,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	#pragma endregion
 
 	#pragma region 宣言と初期化
-	//3Dオブジェクトの初期化
-	std::unique_ptr<Object3d> object = std::make_unique<Object3d>();
-	object->Initialize(objectCommon);
-	object->SetModel("plane.obj");
-	object->SetTranslate({ 0.0f,3.0f,0.0f });
 
-	std::unique_ptr<Object3d> objectAxis = std::make_unique<Object3d>();
-	objectAxis->Initialize(objectCommon);
-	objectAxis->SetModel("axis.obj");
-	objectAxis->SetTranslate({ 5.0f,3.0f,0.0f });
-
-	ParticleManager::GetInstance()->Initialize(dxCommon, srvManager.get());
+	
 	ParticleManager::GetInstance()->CreateParticleGroup("particle", "./Resources/uvChecker.png");
 	#pragma endregion
 
@@ -204,21 +182,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///		>>>汎用機能の更新ここまで<<<		///
 		///////////////////////////////////////////
 
-		//スプライトの更新
-		sprite->Update();
-
-		//3Dオブジェクトの更新
-		object->Update(cameraManager.get());
-		objectAxis->Update(cameraManager.get());
+		//パーティクルの更新
 		ParticleManager::GetInstance()->Update(cameraManager.get());
-		
-
-
-		//スプライト（複数）
-		for(int i = 0; i < spriteCount; ++i)
-		{
-			sprites[i]->Update();
-		}
 
 		
 #ifdef _DEBUG
@@ -226,110 +191,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		#pragma region シーン全体の設定
 		ImGui::Begin("Setting");
-		if(ImGui::Button("Add Particle"))
-		{
-			ParticleManager::GetInstance()->Emit("particle", { 0.0f,3.0f,0.0f }, 5);
-		}
 		ImGui::End();
 		#pragma endregion
 
-		#pragma region
-		ImGui::Begin("CameraManager");
-		if(ImGui::Button("Set Camera"))
-		{
-			object->SetCamera(cameraManager->GetActiveCamera());
-			objectAxis->SetCamera(cameraManager->GetActiveCamera());
-		}
-		ImGui::End();
-		#pragma endregion
 
-		#pragma region スプライト用のImGui
-		ImGui::Begin("Sprite");
-		//スプライトの位置
-		Vector2 position = sprite->GetPosition();
-		ImGui::DragFloat2("translate",&position.x,1.0f,-1280.0f,1280.0f,"%.1f");
-		sprite->SetPosition(position);
-		//スプライトの回転
-		float rotation = sprite->GetRotation();
-		ImGui::SliderAngle("rotate", &rotation, 0.0f, 360.0f);
-		sprite->SetRotation(rotation);
-		//スプライトの色
-		Vector4 color = sprite->GetColor();
-		ImGui::ColorEdit4("color", &color.x);
-		sprite->SetColor(color);
-		//スプライトのサイズ
-		Vector2 size = sprite->GetSize();
-		ImGui::DragFloat2("size", &size.x, 1.0f);
-		sprite->SetSize(size);
-		//スプライトのアンカーポイント
-		Vector2 anchorPoint = sprite->GetAnchorPoint();
-		ImGui::DragFloat2("anchorPoint", &anchorPoint.x, 0.01f, 0.0f, 1.0f);
-		sprite->SetAnchorPoint(anchorPoint);
-		//スプライトの切り出しサイズ
-		Vector2 drawSize = sprite->GetTextureSize();
-		ImGui::DragFloat2("drawSize", &drawSize.x, 1.0f);
-		sprite->SetTextureSize(drawSize);
-		//反転
-		ImGui::Text("Flip");
-		ImGui::SameLine();
-		if(ImGui::Button("FlipX"))
-		{
-			bool flipX = !sprite->GetFlipX();
-			sprite->SetFlipX(flipX);
-		}
-		ImGui::SameLine();
-		if(ImGui::Button("FlipY"))
-		{
-			bool flipY = !sprite->GetFlipY();
-			sprite->SetFlipY(flipY);
-		}
-		//スプライトのテクスチャ
-		ImGui::Text("Change Texture");
-		ImGui::SameLine();
-		if(ImGui::Button("MonsterBall"))
-		{
-			sprite->SetTexture("./Resources/monsterBall.png");
-		}
-		ImGui::SameLine();
-		if(ImGui::Button("UVChecker"))
-		{
-			sprite->SetTexture("./Resources/uvChecker.png");
-		}
-		ImGui::End();
-		#pragma endregion
 
-		#pragma region 3Dオブジェクト用のImGui
-		ImGui::Begin("Object3D");
-		//3Dオブジェクトの位置
-		Vector3 objectPosition = object->GetTranslate();
-		ImGui::DragFloat3("translate", &objectPosition.x, 0.1f, -10.0f, 10.0f);
-		object->SetTranslate(objectPosition);
-		//3Dオブジェクトの回転
-		Vector3 objectRotate = object->GetRotate();
-		ImGui::DragFloat3("rotate", &objectRotate.x, 0.01f,-3.14f,3.14f);
-		object->SetRotate(objectRotate);
-		//3Dオブジェクトの拡大縮小
-		Vector3 objectScale = object->GetScale();
-		ImGui::DragFloat3("scale", &objectScale.x, 0.1f, 0.0f, 10.0f);
-		object->SetScale(objectScale);
-		ImGui::End();
-
-		ImGui::Begin("Object3D Axis");
-		//座標
-		Vector3 objectAxisPosition = objectAxis->GetTranslate();
-		ImGui::DragFloat3("translate", &objectAxisPosition.x, 0.1f, -10.0f, 10.0f);
-		objectAxis->SetTranslate(objectAxisPosition);
-		//回転
-		Vector3 objectAxisRotate = objectAxis->GetRotate();
-		ImGui::DragFloat3("rotate", &objectAxisRotate.x, 0.01f, -3.14f, 3.14f);
-		objectAxis->SetRotate(objectAxisRotate);
-		//拡大縮小
-		Vector3 objectAxisScale = objectAxis->GetScale();
-		ImGui::DragFloat3("scale", &objectAxisScale.x, 0.1f, 0.0f, 10.0f);
-		objectAxis->SetScale(objectAxisScale);
-		ImGui::End();
-
-		#pragma endregion
 
 #endif
 
@@ -354,8 +220,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		//NOTE:3Dオブジェクトの描画準備。共通の設定を行う
 		objectCommon->CommonRenderingSetting();
 
-		object->Draw();
-		objectAxis->Draw();
+
+
 		ParticleManager::GetInstance()->Draw();
 
 		/*--------------[ スプライトの描画 ]-----------------*/
@@ -363,15 +229,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		//スプライトの描画準備。共通の設定を行う
 		spriteCommon->CommonRenderingSetting();
 
-		sprite->Draw();
 
-		for(int i = 0; i < spriteCount; ++i)
-		{
-		//	sprites[i]->Draw();
-		}
-
-		//実際のcommandListのImGuiの描画コマンドを積む
-		//ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), dxCommon->GetCommandList());
 
 		imguiManager->Draw();
 
