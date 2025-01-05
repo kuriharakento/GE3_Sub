8bcbd044ea0254a80a6aa1771eb552182b57e1fa
kento <k023g0033@g.neec.ac.jp>
2025-01-05 17:44:50 +0900

    コリジョンマネージャーのコメント追加

--- project/application/Manager/CollisionManager.h
+++ project/application/Manager/CollisionManager.h
@@ -4,17 +4,23 @@
 
 class CollisionManager {
 public:
+	//当たり判定を行うオブジェクトの追加
     void AddCollidable(ICollidable* collidable);
+	//初期化
 	void Initialize();
+	//更新
     void Update();
+	//リストのクリア
 	void Clear();
 
 private:
+	//当たり判定を行う
     bool CheckCollision(ICollidable* a, ICollidable* b);
 
 	//プレイヤーと建物の当たり判定
 	void HandlePlayerBuildingCollision(ICollidable* a, ICollidable* b);
 
+	//当たり判定を行うオブジェクトのリスト
     std::vector<ICollidable*> collidables_;
 };
 
