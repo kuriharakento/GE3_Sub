ab87a5f765c005a245a86b5d4d379cb5c4dc8647
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2025-01-09 10:42:39 +0900

    一旦オーディオクラスを飛ばして違うものをやる。

--- project/engine/audio/Audio.cpp
+++ project/engine/audio/Audio.cpp
@@ -1,97 +1,97 @@
-#include "Audio.h"
-
-#include <cassert>
-
-void Audio::Initialize()
-{
-	//XAudio2エンジンのインスタンスを生成
-	XAudio2Create(&xAudio2,0,XAUDIO2_DEFAULT_PROCESSOR);
-	//マスターボイスを生成
-	xAudio2->CreateMasteringVoice(&masterVoice);
-}
-
-SoundData Audio::LoadWave(const char* filename)
-{
-	HRESULT result;
-
-	/*--------------[ ファイルの読み込み ]-----------------*/
-
-	//ファイル入力ストリームのインスタンスを生成
-	std::ifstream file;
-	//バイナリモードでファイルを開く
-	file.open(filename, std::ios::binary);
-	//ファイルが開けなかった場合
-	assert(file.is_open());
-
-	/*--------------[ wavデータ読み込み ]-----------------*/
-
-	//RIFFヘッダの読み込み
-	RiffHeader riff;
-	file.read((char*)&riff, sizeof(riff));
-	//ファイルがRIFFかチェック
-	if (strncmp(riff.chunk.id, "RIFF", 4) != 0)
-	{
-		assert(0);
-	}
-	//タイプがWAVEかチェック
-	if (strncmp(riff.type, "WAVE", 4) != 0)
-	{
-		assert(0);
-	}
-
-	//Formatチャンクの読み込み
-	FormatChunk format = {};
-	//チャンクヘッダーの確認
-	file.read((char*)&format, sizeof(ChunkHeader));
-	if (strncmp(format.chunk.id, "fmt ", 4) != 0)
-	{
-		assert(0);
-	}
-
-	//チャンク本体の読み込み
-	assert(format.chunk.size <= sizeof(format.fmt));
-	file.read((char*)&format.fmt, format.chunk.size);
-
-	//Dataチャンクの読み込み
-	ChunkHeader data;
-	file.read((char*)&data, sizeof(data));
-	//JUNKチャンクを検出した場合
-	if (strncmp(data.id, "JUNK", 4) == 0)
-	{
-		//JUNKチャンクをスキップ
-		file.seekg(data.size, std::ios::cur);
-		//次のチャンクを読み込む
-		file.read((char*)&data, sizeof(data));
-	}
-
-	if (strncmp(data.id, "data", 4) != 0)
-	{
-		assert(0);
-	}
-
-	//Dataチャンクのデータ部分を読み込む
-	char* buffer = new char[data.size];
-	file.read(buffer, data.size);
-
-	//waveファイルを閉じる
-	file.close();
-
-	/*--------------[ 読み込んだ音声データをreturn ]-----------------*/
-
-	SoundData soundData = {};
-	soundData.wfex = format.fmt;
-	soundData.pBuffer = reinterpret_cast<BYTE*>(buffer);
-	soundData.bufferSize = data.size;
-
-	return soundData;
-}
-
-void Audio::SoundUnload(SoundData* soundData)
-{
-	delete[] soundData->pBuffer;
-
-	soundData->pBuffer = nullptr;
-	soundData->bufferSize = 0;
-	soundData->wfex = {};
-
-}
+//#include "Audio.h"
+//
+//#include <cassert>
+//
+//void Audio::Initialize()
+//{
+//	//XAudio2エンジンのインスタンスを生成
+//	XAudio2Create(&xAudio2,0,XAUDIO2_DEFAULT_PROCESSOR);
+//	//マスターボイスを生成
+//	xAudio2->CreateMasteringVoice(&masterVoice);
+//}
+//
+//SoundData Audio::LoadWave(const char* filename)
+//{
+//	HRESULT result;
+//
+//	/*--------------[ ファイルの読み込み ]-----------------*/
+//
+//	//ファイル入力ストリームのインスタンスを生成
+//	std::ifstream file;
+//	//バイナリモードでファイルを開く
+//	file.open(filename, std::ios::binary);
+//	//ファイルが開けなかった場合
+//	assert(file.is_open());
+//
+//	/*--------------[ wavデータ読み込み ]-----------------*/
+//
+//	//RIFFヘッダの読み込み
+//	RiffHeader riff;
+//	file.read((char*)&riff, sizeof(riff));
+//	//ファイルがRIFFかチェック
+//	if (strncmp(riff.chunk.id, "RIFF", 4) != 0)
+//	{
+//		assert(0);
+//	}
+//	//タイプがWAVEかチェック
+//	if (strncmp(riff.type, "WAVE", 4) != 0)
+//	{
+//		assert(0);
+//	}
+//
+//	//Formatチャンクの読み込み
+//	FormatChunk format = {};
+//	//チャンクヘッダーの確認
+//	file.read((char*)&format, sizeof(ChunkHeader));
+//	if (strncmp(format.chunk.id, "fmt ", 4) != 0)
+//	{
+//		assert(0);
+//	}
+//
+//	//チャンク本体の読み込み
+//	assert(format.chunk.size <= sizeof(format.fmt));
+//	file.read((char*)&format.fmt, format.chunk.size);
+//
+//	//Dataチャンクの読み込み
+//	ChunkHeader data;
+//	file.read((char*)&data, sizeof(data));
+//	//JUNKチャンクを検出した場合
+//	if (strncmp(data.id, "JUNK", 4) == 0)
+//	{
+//		//JUNKチャンクをスキップ
+//		file.seekg(data.size, std::ios::cur);
+//		//次のチャンクを読み込む
+//		file.read((char*)&data, sizeof(data));
+//	}
+//
+//	if (strncmp(data.id, "data", 4) != 0)
+//	{
+//		assert(0);
+//	}
+//
+//	//Dataチャンクのデータ部分を読み込む
+//	char* buffer = new char[data.size];
+//	file.read(buffer, data.size);
+//
+//	//waveファイルを閉じる
+//	file.close();
+//
+//	/*--------------[ 読み込んだ音声データをreturn ]-----------------*/
+//
+//	SoundData soundData = {};
+//	soundData.wfex = format.fmt;
+//	soundData.pBuffer = reinterpret_cast<BYTE*>(buffer);
+//	soundData.bufferSize = data.size;
+//
+//	return soundData;
+//}
+//
+//void Audio::SoundUnload(SoundData* soundData)
+//{
+//	delete[] soundData->pBuffer;
+//
+//	soundData->pBuffer = nullptr;
+//	soundData->bufferSize = 0;
+//	soundData->wfex = {};
+//
+//}
--- project/engine/audio/Audio.h
+++ project/engine/audio/Audio.h
@@ -1,53 +1,53 @@
-#pragma once
-#include <xaudio2.h>
-#pragma comment(lib, "xaudio2.lib")
-#include <fstream>
-#include <wrl.h>
-
-//チャンクヘッダ
-struct ChunkHeader
-{
-	char id[4];					//チャンクID
-	int32_t size;				//チャンクサイズ
-};
-
-//RIFFヘッダ
-struct RiffHeader
-{
-	ChunkHeader chunk;			//RIFF
-	char type[4];				//WAVE
-};
-
-//FMTチャンク
-struct FormatChunk
-{
-	ChunkHeader chunk;
-	WAVEFORMATEX fmt;			//波形フォーマット
-};
-
-//音声データ
-struct SoundData
-{
-	WAVEFORMATEX wfex;			//波形フォーマット
-	BYTE* pBuffer;				//バッファの先頭アドレス
-	unsigned int bufferSize;	//バッファのサイズ
-};
-
-class Audio
-{
-public:
-	void Initialize();
-
-	SoundData LoadWave(const char* filename);
-	void SoundUnload(SoundData* soundData);
-
-	//解放
-	void SoundUnload(SoundData* soundData);
-
-private:
-	//IXAudio2
-	Microsoft::WRL::ComPtr<IXAudio2> xAudio2;
-	//IXAudio2MasteringVoice
-	IXAudio2MasteringVoice* masterVoice;
-};
-
+//#pragma once
+//#include <xaudio2.h>
+//#pragma comment(lib, "xaudio2.lib")
+//#include <fstream>
+//#include <wrl.h>
+//
+////チャンクヘッダ
+//struct ChunkHeader
+//{
+//	char id[4];					//チャンクID
+//	int32_t size;				//チャンクサイズ
+//};
+//
+////RIFFヘッダ
+//struct RiffHeader
+//{
+//	ChunkHeader chunk;			//RIFF
+//	char type[4];				//WAVE
+//};
+//
+////FMTチャンク
+//struct FormatChunk
+//{
+//	ChunkHeader chunk;
+//	WAVEFORMATEX fmt;			//波形フォーマット
+//};
+//
+////音声データ
+//struct SoundData
+//{
+//	WAVEFORMATEX wfex;			//波形フォーマット
+//	BYTE* pBuffer;				//バッファの先頭アドレス
+//	unsigned int bufferSize;	//バッファのサイズ
+//};
+//
+//class Audio
+//{
+//public:
+//	void Initialize();
+//
+//	SoundData LoadWave(const char* filename);
+//	void SoundUnload(SoundData* soundData);
+//
+//	//解放
+//	void SoundUnload(SoundData* soundData);
+//
+//private:
+//	//IXAudio2
+//	Microsoft::WRL::ComPtr<IXAudio2> xAudio2;
+//	//IXAudio2MasteringVoice
+//	IXAudio2MasteringVoice* masterVoice;
+//};
+//
