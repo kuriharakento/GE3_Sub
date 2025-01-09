f39f82ac1c481efc8c4ad755319155cc1755949a
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2025-01-09 16:05:08 +0900

    インターフェイスクラスの作成

---
+++ project/application/scene/IScene.h
@@ -0,0 +1,19 @@
+#pragma once
+class IScene
+{
+public: //メンバ関数
+	//デストラクタ
+	virtual ~IScene() = default;
+
+	//初期化
+	virtual void Initialize() = 0;
+	//終了
+	virtual void Finalize() = 0;
+	//更新
+	virtual void Update() = 0;
+	//描画
+	virtual void Draw3D();
+	virtual void Draw2D();
+	
+};
+
