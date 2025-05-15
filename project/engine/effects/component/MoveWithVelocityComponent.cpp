0d223fa6e08700edb6bdd58abcaeb05b655b7ed2
kento <k023g0033@g.neec.ac.jp>
2025-05-16 00:39:20 +0900

    試しに実装したコンポ―ネントすべてをエミッタにすべて追加してみた

--- project/engine/effects/component/MoveWithVelocityComponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "MoveWithVelocityComponent.h"
-
-void MoveWithVelocityComponent::Update(Particle& particle)
-{
-	particle.transform.translate += particle.velocity * (1.0f / 60.0f); // 1フレーム分の時間を加算
-}
--- project/engine/effects/component/MoveWithVelocityComponent.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "IParticleBehaviorComponent.h"
-
-class MoveWithVelocityComponent : public IParticleBehaviorComponent
-{
-public:
-    void Update(Particle& particle) override;
-};
