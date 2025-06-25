7fb831ffe6e4e2c6fa50745a333e3afb31dd704c
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2025-06-25 17:02:04 +0900

    GameObjectManagerの作成途中。

---
+++ project/application/GameObject/base/GameObjectManager.cpp
@@ -0,0 +1,14 @@
+#include "GameObjectManager.h"
+
+static GameObjectManager* instance_ = nullptr;
+
+GameObjectManager* GameObjectManager::GetInstance()
+{
+	if (instance_ == nullptr)
+	{
+		instance_ = new GameObjectManager();
+	}
+	return instance_;
+}
+
+
---
+++ project/application/GameObject/base/GameObjectManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "GameObject.h"
+class CameraManager;
+
+class GameObjectManager
+{
+public:
+	// シングルトン
+	static GameObjectManager* GetInstance();
+	// 初期化
+	void Initialize();
+	// 終了
+	void Finalize();
+	// 全てのGameObjectを更新
+	void Update();
+	// 全てのGameObjectを描画
+	void Draw(CameraManager* camera);
+	// GameObjectのポインタを登録する
+	void Register(GameObject* gameObject);
+	// GameObjectのポインタをリストから削除する
+	void Unregister(GameObject* gameObject);
+
+private:
+	// GameObjectのリスト
+	std::vector<GameObject*> gameObjects_;
+};
+
